9159edc3fd932b48756494d6d2afb959293f0d08
tune <hisatsune01@gmail.com>
2020-08-02 15:16:36 +0900

    study storybook

---
+++ src/components/Example/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useCallback } from 'react';
+
+export interface ExampleProps {
+  text: string;
+  flag?: boolean;
+  action(): void;
+}
+
+const Example = (props: ExampleProps) => {
+  const { text, flag, action } = props;
+  const [count, countChg] = useState(0);
+  const countUp = useCallback(() => countChg((prev) => prev + 1), []);
+  const countDown = useCallback(() => countChg((prev) => prev - 1), []);
+
+  return (
+    <div>
+      {flag && <p>{text}</p>}
+      <button onClick={action}>ボタン</button>
+      <p>count:{count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>+</button>
+    </div>
+  );
+};
+
+export default Example;
